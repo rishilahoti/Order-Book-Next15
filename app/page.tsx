a1f4241650fe2684d42f6bc255251cba652c1681
rishilahoti <rishilahoti99@gmail.com>
2024-11-17 23:25:12 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,12 @@ export const ProductIds = {
 	ETHUSD: 'PI_ETHUSD',
 };
 
-const options: any = {
+const options: { [key: string]: number[] } = {
 	PI_XBTUSD: [0.5, 1, 2.5],
 	PI_ETHUSD: [0.05, 0.1, 0.25],
 };
 
-export const ProductsMap: any = {
+export const ProductsMap: { [key: string]: string } = {
 	PI_XBTUSD: 'PI_ETHUSD',
 	PI_ETHUSD: 'PI_XBTUSD',
 };
